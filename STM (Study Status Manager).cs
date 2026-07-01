b9ffde35db87b15e13fbf0fb560ff966618cc275
StefanyME <marazostefany0@hotmail.com>
2026-07-01 16:06:34 -0300

    Update STM (Study Status Manager).cs

--- STM (Study Status Manager).cs
+++ STM (Study Status Manager).cs
@@ -1,32 +1,34 @@
 using System;
-// Basic tools of entry and exit of info
+// Basic tools for entry and exit of info
+
 using System.IO;
 // Input/Output - specific tool to use codes related to computer files
 
-class Program{
-// Name of the code or class, every program algoritm on C# may have a class (mainly = Program)
-    static void Main() {
-    // Starting point of the algorithm, just do the task without math values
-        Console.Write("What's the studied subject?");
-        // Makes the question appear on the console to be answered
-        string subject = Console.ReadLine();
-        //Reads the info and save it on a variable, cannot have anything inside the "()"
-        // String specifies that the variable type: text. Every variable has a type on C#
+class Program {
+// Name of the code or class. Every program algorithm in C# must have a class (usually = Program)
 
-        Console.Write("What's the progress percentage?");
+    static void Main() {
+    // Starting point of the algorithm, just do the task without returning values
+    
+        Console.Write("What's the studied subject? ");
+        // Makes the question appear on the console to be answered
+        
+        string subject = Console.ReadLine();
+        // Reads the info and saves it in a variable. Cannot have anything inside the "()"
+        // String specifies the variable type: text. Every variable has a type in C#
+
+        Console.Write("What's the progress percentage? ");
         string percentage = Console.ReadLine();
 
-        Console.Write("What's the next task?");
+        Console.Write("What's the next task? ");
         string next_task = Console.ReadLine();
+        // Console.ReadLine is like the input() command in Python
 
-        // Console.ReadLine is like the command input on Python
-
-        File.AppendAllText("my_progress.txt", $"Subject:{subject}, Progress:{percentage}%, Next Task:{next_task}n/");
-        // Console.Write is basically the command print on Python, but it needs another command to read the text
-        // /n = New line | Enter
+        File.AppendAllText("my_progress.txt", $"Subject: {subject}, Progress: {percentage}%, Next Task: {next_task}\n");
+        // Console.Write is basically the print() command in Python, but without the automatic newline
+        // \n = New line | Enter
     }
-
 }
 
-// All the code lines must end with a semicolon ;.
-// $ works like a f on Python, is to indicate the existence of variables on the line of text (resulting on the need to get the info collected of these variables)
+// All code lines inside methods must end with a semicolon ;
+// $ works like 'f' in Python, used to indicate the existence of variables inside the text line
